446ddbd12409caaea8360be7ad0ffea26638eba7
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-05-13 23:35:52 +0200

    fixed code long and other things

--- app/main.py
+++ app/main.py
@@ -1,32 +1,22 @@
 def format_linter_error(error: dict) -> dict:
-
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8",
+        "line": error["line_number"], "column": error["column_number"],
+        "message": error["text"], "name": error["code"], "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
     return {
-        "errors": [
-            format_linter_error(error) for error in errors
-        ],
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed"
+        "status": "failed" if errors else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-
     return [
-        {
-            "errors": [],
-            "path": path,
-            "status": "passed",
-        } if not report else format_single_linter_file(path, report)
+        {"errors": [], "path": path, "status": "passed"}
+        if not report
+        else format_single_linter_file(path, report)
         for path, report in linter_report.items()
     ]
